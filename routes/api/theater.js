8decd72c8d6b44dae421da09bf5f77bbdca6a36e
Chris Padilla <cdpadilla42@gmail.com>
2020-07-31 13:15:24 -0500

    Add sanitization and validation to theater and movie POST requests

--- routes/api/theater.js
+++ routes/api/theater.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../../models/Movie');
 const Theater = require('../../models/Theater');
+const { body, sanitizeBody, validationResult } = require('express-validator');
 
 // @route   GET api/:theater
 // @desc    Get all theaters
@@ -16,36 +17,74 @@ router.get('/', async (req, res) => {
 // @route   POST api/:theater
 // @desc    Post new theater
 // TODO     @acces   Private
-router.post('/', async (req, res) => {
-  const newTheater = await new Theater(req.body)
-    .save()
-    .catch((err) => res.json({ success: false }));
-  res.json(newTheater);
-});
+router.post('/', [
+  // Validate
+  body('name', 'Name Required').trim().isLength({ min: 1 }),
+
+  // Sanitize
+  sanitizeBody('name').escape(),
+
+  // Process
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ errors });
+    }
+
+    const newTheater = await new Theater(req.body)
+      .save()
+      .catch((err) => res.json({ success: false }));
+    res.json(newTheater);
+  },
+]);
 
 // @route   GET api/:theater
 // @desc    Get all movies at a theater
 // @acces   Public
-router.get('/:theater', async (req, res) => {
-  // res.json({ yes: true });
-  const theater = await Theater.find({ name: req.params.theater });
-  const movies = await Movie.find({ theater }).sort({ date: -1 });
-  res.json(movies);
-});
+router.get('/:theater', [
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ errors });
+    }
+
+    const theater = await Theater.find({ name: req.params.theater });
+    const movies = await Movie.find({ theater }).sort({ date: -1 });
+    res.json(movies);
+  },
+]);
 
 // @route   POST api/:theater
 // @desc    post new movie
 // TODO     @acces Private
-router.post('/:theater', async (req, res) => {
-  // res.json(req.body);
-  const theater = await Theater.findOne({ name: req.params.theater });
-  // res.json({ theater: theater });
-  // res.json({ theater: theater._id, ...req.body });
-  const newMovie = await new Movie({ ...req.body, theater: theater._id })
-    .save()
-    .catch((err) => res.json({ message: err }));
-  res.json(newMovie);
-});
+router.post('/:theater', [
+  // Validate
+  body('name', 'Name required').trim().isLength({ min: 1 }),
+  body('desc', 'Description required').trim().isLength({ min: 1 }),
+  body('image', 'Image required').trim().isLength({ min: 1 }),
+  body('price', 'Price required').trim().isLength({ min: 1 }),
+
+  // Sanitize
+  sanitizeBody('name').escape(),
+  sanitizeBody('desc').escape(),
+  sanitizeBody('image').escape(),
+  sanitizeBody('price').escape(),
+
+  // process request
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ errors });
+    }
+
+    const theater = await Theater.findOne({ name: req.params.theater });
+    // res.json({ theat: theater._id, ...req.body });
+    const newMovie = await new Movie({ ...req.body, theater: theater._id })
+      .save()
+      .catch((err) => res.json({ message: err }));
+    res.json(newMovie);
+  },
+]);
 
 // @route   PUT api/:theater/:movie
 // @desc    update movie
