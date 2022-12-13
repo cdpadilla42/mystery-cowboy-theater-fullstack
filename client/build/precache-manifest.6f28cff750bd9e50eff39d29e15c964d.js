85523a625bbd8b7ba290a2c84f5ddb8227a77b62
Chris Padilla <cdpadilla42@gmail.com>
2022-12-13 15:55:12 -0600

    Build the app

---
+++ client/build/precache-manifest.6f28cff750bd9e50eff39d29e15c964d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026b65e7ed6a56f7494f229043f120fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5132f8b67e6e9283d80",
+    "url": "/static/css/main.121ce1a7.chunk.css"
+  },
+  {
+    "revision": "d397cfb1ba81b292862a",
+    "url": "/static/js/2.6709791e.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.6709791e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5132f8b67e6e9283d80",
+    "url": "/static/js/main.2e8ac0f6.chunk.js"
+  },
+  {
+    "revision": "acc64e4da07f1a2340bd",
+    "url": "/static/js/runtime-main.3c60fe56.js"
+  }
+]);
